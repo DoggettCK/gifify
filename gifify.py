bd343917bafbe16c74863281fe12f6e29851d180
Chris Doggett <doggettc@gmail.com>
2025-03-18 11:21:20 -0500

    Cleanup with linter/formatter

--- gifify.py
+++ gifify.py
@@ -2,21 +2,49 @@
 import argparse
 import os
 import os.path
-import re
 import subprocess
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert a movie to GIF with optional subtitles')
+    parser = argparse.ArgumentParser(
+        description="Convert a movie to GIF with optional subtitles"
+    )
 
     parser.set_defaults(dry_run=False)
     parser.set_defaults(duration=5)
-    parser.set_defaults(timestamp='00:00:00')
-    parser.add_argument('-i, --input', dest='input', action='store', required=True, help='input video file')
-    parser.add_argument('-o, --output', dest='output', action='store', required=True, help='output GIF file')
-    parser.add_argument('-s, --subtitles', dest='subtitles', action='store', help='file containing subtitles')
-    parser.add_argument('-t, --timestamp', dest='timestamp', action='store', help='timestamp to start GIF from in HH:MM:SS format')
-    parser.add_argument('-d, --duration', dest='duration', action='store', help='length of GIF in seconds')
-    parser.add_argument('-n, --dry_run', dest='dry_run', action='store_true')
+    parser.set_defaults(timestamp="00:00:00")
+    parser.add_argument(
+        "-i, --input",
+        dest="input",
+        action="store",
+        required=True,
+        help="input video file",
+    )
+    parser.add_argument(
+        "-o, --output",
+        dest="output",
+        action="store",
+        required=True,
+        help="output GIF file",
+    )
+    parser.add_argument(
+        "-s, --subtitles",
+        dest="subtitles",
+        action="store",
+        help="file containing subtitles",
+    )
+    parser.add_argument(
+        "-t, --timestamp",
+        dest="timestamp",
+        action="store",
+        help="timestamp to start GIF from in HH:MM:SS format",
+    )
+    parser.add_argument(
+        "-d, --duration",
+        dest="duration",
+        action="store",
+        help="length of GIF in seconds",
+    )
+    parser.add_argument("-n, --dry_run", dest="dry_run", action="store_true")
 
     args = parser.parse_args()
 
@@ -24,38 +52,36 @@ if __name__ == "__main__":
         print(f"ERROR: Input file ({args.input}) not found")
         exit(1)
 
-    if args.subtitles != None and not os.path.exists(args.subtitles):
+    if args.subtitles is not None and not os.path.exists(args.subtitles):
         print(f"ERROR: Subtitles file ({args.subtitles}) specified, but not found")
         exit(1)
 
     try:
         args.duration = float(args.duration)
     except ValueError:
-        print(f"ERROR: Duration must be a number")
+        print("ERROR: Duration must be a number")
         exit(1)
 
     process_args = [
-            "ffmpeg",
-            "-i",
-            f"\"{args.input}\"",
-            "-vf",
-            f"\"subtitles=\\'{args.subtitles}\\'\"",
-            "-ss",
-            f"{args.timestamp}",
-            "-t",
-            f"{args.duration}",
-            "-y",
-            f"\"{args.output}\""
-            ]
+        "ffmpeg",
+        "-i",
+        f'"{args.input}"',
+        "-vf",
+        f"\"subtitles=\\'{args.subtitles}\\'\"",
+        "-ss",
+        f"{args.timestamp}",
+        "-t",
+        f"{args.duration}",
+        "-y",
+        f'"{args.output}"',
+    ]
 
-    if args.subtitles == None:
-        del(process_args[3:5])
+    if args.subtitles is None:
+        del process_args[3:5]
 
-    if args.dry_run:
-        print(" ".join(process_args))
-    else:
+    print(" ".join(process_args))
+    if not args.dry_run:
         try:
-            subprocess.run(process_args, cwd=os.getcwd())
+            subprocess.run(process_args, shell=True, cwd=os.getcwd())
         except FileNotFoundError:
             print("ERROR: ffmpeg not installed")
-
